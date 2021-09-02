3ece0ad989e709409d7c59c86453ed98ccffd4ee
evahong-21 <evahong94@gmail.com>
2021-09-02 12:56:19 +0900

    'Command_product

---
+++ app/src/main/java/com/eomcs/pms/handler/ProductDetailHandler.java
@@ -0,0 +1,46 @@
+package com.eomcs.pms.handler;
+
+import java.util.List;
+import com.eomcs.pms.domain.Product;
+import com.eomcs.util.Prompt;
+
+public class ProductDetailHandler extends AbstractProductHandler {
+
+  public ProductDetailHandler(List<Product> alcoholList) {
+    super(alcoholList);
+  }
+
+  public void detail() {
+    System.out.println("[상품 상세보기]");
+
+    Product product = findByProduct(Prompt.inputString("상품명 : "));
+
+    if (product == null) {
+      System.out.println("입력하신 상품이 없습니다.");
+      return;
+    }
+
+    System.out.printf("상품명: %s\n", product.getProductName());
+    System.out.printf("주종: %s\n", product.getProductType());
+    System.out.printf("원산지: %s\n", product.getCountryOrigin());
+    System.out.printf("품종: %s\n", product.getVariety());
+    System.out.printf("알콜도수: %.2f\n", product.getAlcoholLevel());
+    System.out.printf("당도: %d\n", product.getSugerLevel());
+    System.out.printf("산도: %d\n", product.getAcidity());
+    System.out.printf("바디감: %d\n", product.getWeight());
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/eomcs/pms/handler/StockUpdateHandler.java
@@ -0,0 +1,51 @@
+package com.eomcs.pms.handler;
+
+import java.util.List;
+import com.eomcs.pms.App;
+import com.eomcs.pms.domain.Stock;
+import com.eomcs.util.Prompt;
+
+public class StockUpdateHandler extends AbstractStockHandler {
+
+  public StockUpdateHandler(List<Stock> stockList) {
+    super(stockList);
+  }
+
+  public void update() {
+    if (App.getLoginUser().getAuthority() == 0 || App.getLoginUser().getAuthority() == 1 ) {
+      System.out.println("해당 메뉴는 판매자 권한입니다.");
+      return;
+    }
+    while(true) {
+      System.out.println("[재고 변경]");
+
+      Stock stock = findByStock(Prompt.inputString("상품명 : "));
+
+      if (stock == null) {
+        System. out.println("해당 상품의 재고가 없습니다.");
+        return;
+      }
+
+      int stocks = Prompt.inputInt("재고수량(" + stock.getStocks() + ") :");
+      int price = Prompt.inputInt("가격(" + stock.getPrice() + ") : ");
+
+      String input = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
+
+      if (input.equalsIgnoreCase("y")) {
+        stock.setPrice(stocks);
+        stock.setPrice(price);
+        System.out.println("재고정보를 변경하였습니다.");
+        return;
+      } else {
+        System.out.println("재고 변경을 취소하였습니다.");
+        return;
+      } 
+    }
+  }
+}
+
+
+
+
+
+
