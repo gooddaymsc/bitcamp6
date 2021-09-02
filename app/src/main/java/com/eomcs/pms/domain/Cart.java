be7c842d6eeededbfbcc7781baf085ac7267ea12
jylion23 <jylion23@github.com>
2021-09-02 13:16:54 +0900

    yj_CartHandler

--- app/src/main/java/com/eomcs/pms/domain/Cart.java
+++ app/src/main/java/com/eomcs/pms/domain/Cart.java
@@ -3,48 +3,37 @@ package com.eomcs.pms.domain;
 import java.sql.Date;
 
 public class Cart {
-  private int cartNumber;
-  private String productName;
-  private String productType;
-  private String countryOrigin;
-  private String productPhoto;
-  private String price;
+  private Stock stock; // 장바구니 담을 상품
+  private int cartStocks; // 장바구니상품의 갯수
+  private int cartPrice; // 총금액
+  private int cartNumber; //장바구니번호
   private Date registeredDate;
+
+
+
+  public int getCartPrice() {
+    return cartPrice;
+  }
+  public void setCartPrice(int cartPrice) {
+    this.cartPrice = stock.getPrice()*getCartStocks();
+  }
+  public int getCartStocks() {
+    return cartStocks;
+  }
+  public void setCartStocks(int cartStocks) {
+    this.cartStocks = cartStocks;
+  }
+  public Stock getStock() {
+    return stock;
+  }
+  public void setStock(Stock stock) {
+    this.stock = stock;
+  }
   public int getCartNumber() {
     return cartNumber;
   }
   public void setCartNumber(int cartNumber) {
     this.cartNumber = cartNumber;
-  }
-  public String getProductName() {
-    return productName;
-  }
-  public void setProductName(String productName) {
-    this.productName = productName;
-  }
-  public String getProductType() {
-    return productType;
-  }
-  public void setProductType(String productType) {
-    this.productType = productType;
-  }
-  public String getCountryOrigin() {
-    return countryOrigin;
-  }
-  public void setCountryOrigin(String countryOrigin) {
-    this.countryOrigin = countryOrigin;
-  }
-  public String getProductPhoto() {
-    return productPhoto;
-  }
-  public void setProductPhoto(String productPhoto) {
-    this.productPhoto = productPhoto;
-  }
-  public String getPrice() {
-    return price;
-  }
-  public void setPrice(String price) {
-    this.price = price;
   }
   public Date getRegistrationDate() {
     return registeredDate;
