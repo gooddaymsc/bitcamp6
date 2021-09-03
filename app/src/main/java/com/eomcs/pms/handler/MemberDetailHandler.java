47816ddec1a3fbebc417e5d322e678d722a62e55
jylion23 <jylion23@github.com>
2021-09-03 14:52:01 +0900

    memberStock

--- app/src/main/java/com/eomcs/pms/handler/MemberDetailHandler.java
+++ app/src/main/java/com/eomcs/pms/handler/MemberDetailHandler.java
@@ -1,6 +1,7 @@
 package com.eomcs.pms.handler;
 
 import java.util.List;
+import com.eomcs.menu.Menu;
 import com.eomcs.pms.App;
 import com.eomcs.pms.domain.Privacy;
 import com.eomcs.pms.domain.SellerPrivacy;
@@ -12,8 +13,9 @@ public class MemberDetailHandler extends AbstractMemberHandler {
     super(memberList, sellerList);
   }
 
+  @Override
   public void execute(int mem) { 
-    if (App.getLoginUser().getAuthority() != 3) {
+    if (App.getLoginUser().getAuthority() != Menu.ACCESS_ADMIN) {
       System.out.println("권한이 없습니다. 관리자 기능입니다.");
       return;
     }
@@ -21,7 +23,7 @@ public class MemberDetailHandler extends AbstractMemberHandler {
     System.out.println("[회원 상세보기]");
 
     if (mem == 1) {
-      int no = Prompt.inputInt("회원번호를 입력해주세요: ");
+      int no = Prompt.inputInt("회원번호 : ");
 
       Privacy member = findByNo(no);
 
@@ -30,19 +32,19 @@ public class MemberDetailHandler extends AbstractMemberHandler {
         return;
       }
 
-      System.out.printf("이름: %s입니다.\n", member.getName());
-      System.out.printf("닉네임: %s입니다.\n", member.getNickname());
-      System.out.printf("등급: %s입니다.\n", member.getLevel());
-      System.out.printf("이메일: %s입니다.\n", member.getEmail());
-      System.out.printf("사진: %s입니다.\n", member.getPhoto());
-      System.out.printf("전화: %s입니다.\n", member.getPhoneNumber());
+      System.out.printf("이름: %s\n", member.getName());
+      System.out.printf("닉네임: %s\n", member.getNickname());
+      System.out.printf("등급: %s\n", member.getLevel());
+      System.out.printf("이메일: %s\n", member.getEmail());
+      System.out.printf("사진: %s\n", member.getPhoto());
+      System.out.printf("전화: %s\n", member.getPhoneNumber());
       //      System.out.printf("주소: %s입니다.\n", member.getAddress()); // 프라이버시입력?
-      System.out.printf("등록일: %s입니다.\n", member.getRegisteredDate());
+      System.out.printf("등록일: %s\n", member.getRegisteredDate());
 
     }
 
     if (mem == 2) {
-      int no = Prompt.inputInt("판매자번호를 입력해주세요: ");
+      int no = Prompt.inputInt("판매자번호 : ");
 
       SellerPrivacy member = findByNo2(no); 
 
@@ -51,15 +53,15 @@ public class MemberDetailHandler extends AbstractMemberHandler {
         return;
       } 
 
-      System.out.printf("이름: %s입니다.\n", member.getName());
-      System.out.printf("닉네임: %s입니다.\n", member.getNickname());
-      System.out.printf("등급: %s입니다.\n", member.getLevel());
-      System.out.printf("이메일: %s입니다.\n", member.getEmail());
-      System.out.printf("사진: %s입니다.\n", member.getPhoto());
-      System.out.printf("전화: %s입니다.\n", member.getPhoneNumber());
-      System.out.printf("사업자번호: %s입니다.\n", member.getBusinessNumber());
-      System.out.printf("사업장주소: %s입니다.\n", member.getBusinessAddress());
-      System.out.printf("사업장번호: %s입니다.\n", member.getBusinessPlaceNumber());
+      System.out.printf("이름: %s\n", member.getName());
+      System.out.printf("닉네임: %s\n", member.getNickname());
+      System.out.printf("등급: %s\n", member.getLevel());
+      System.out.printf("이메일: %s\n", member.getEmail());
+      System.out.printf("사진: %s\n", member.getPhoto());
+      System.out.printf("전화: %s\n", member.getPhoneNumber());
+      System.out.printf("사업자번호: %\n", member.getBusinessNumber());
+      System.out.printf("사업장주소: %\n", member.getBusinessAddress());
+      System.out.printf("사업장번호: %\n", member.getBusinessPlaceNumber());
     }
   }
 }
--- app/src/main/java/com/eomcs/pms/handler/StockUpdateHandler.java
+++ app/src/main/java/com/eomcs/pms/handler/StockUpdateHandler.java
@@ -1,6 +1,7 @@
 package com.eomcs.pms.handler;
 
 import java.util.List;
+import com.eomcs.menu.Menu;
 import com.eomcs.pms.App;
 import com.eomcs.pms.domain.Stock;
 import com.eomcs.util.Prompt;
@@ -14,22 +15,22 @@ public class StockUpdateHandler extends AbstractStockHandler {
 
   @Override
   public void execute() {
-    if (App.getLoginUser().getAuthority() == 0 || App.getLoginUser().getAuthority() == 1 ) {
+    if (App.getLoginUser().getAuthority() == Menu.ACCESS_SELLER) {
       System.out.println("해당 메뉴는 판매자 권한입니다.");
       return;
     }
     while(true) {
       System.out.println("[재고 변경]");
 
-      Stock stock = findByStock(Prompt.inputString("상품명 : "));
+      Stock stock = findByStock(Prompt.inputString("변경할 상품명 : "));
 
       if (stock == null) {
         System. out.println("해당 상품의 재고가 없습니다.");
         return;
       }
 
-      int stocks = Prompt.inputInt("재고수량(" + stock.getStocks() + ") :");
-      int price = Prompt.inputInt("가격(" + stock.getPrice() + ") : ");
+      int stocks = Prompt.inputInt(String.format("수량(변경 전 : %d) : ", stock.getStocks()));
+      int price = Prompt.inputInt(String.format("가격(변경 전 : %d) : ", stock.getPrice()));
 
       String input = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
 
