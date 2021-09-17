b18356be11ed7b21bc82dbf82702a14aa8d5cf7d
Sumin-yun <sumin.yun73@gmail.com>
2021-09-17 17:39:45 +0900

    ranking5

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -167,7 +167,7 @@ public class App {
 
     commandMap.put("/product/add",    new ProductAddHandler(productList, productPrompt));
     commandMap.put("/product/list",   new ProductListHandler(stockPrompt, productPrompt, cartPrompt, productList, allStockList, allCartList, memberPrompt));
-    commandMap.put("/product/search", new ProductSearchHandler(productPrompt, stockPrompt, memberPrompt, cartPrompt));
+    commandMap.put("/product/search", new ProductSearchHandler(productPrompt, stockPrompt, memberPrompt, cartPrompt, productList));
 
     commandMap.put("/product/detail", new ProductDetailHandler(productPrompt, productList));
     commandMap.put("/product/update", new ProductUpdateHandler(productPrompt));
@@ -286,6 +286,7 @@ public class App {
     }
   }
 
+
   Menu createMenu() {
 
     MenuGroup mainMenuGroup = new MenuGroup("메인");
@@ -376,10 +377,10 @@ public class App {
 
     ///////////////////////////////////////////
 
-    MenuGroup rankingMenu = new MenuGroup("이달의 술");
+    MenuGroup rankingMenu = new MenuGroup("실시간 랭킹");
     mainMenuGroup.add(rankingMenu);
 
-    rankingMenu.add(new MenuItem("이달의 술",  "/ranking/list"));
+    rankingMenu.add(new MenuItem("실시간 랭킹",  "/ranking/list"));
 
     ///////////////////////////////////////////
 
