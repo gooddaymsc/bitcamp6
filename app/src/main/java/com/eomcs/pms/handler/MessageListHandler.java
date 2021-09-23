ddd1dcf1455a27c59c90b2f38c07072276f13487
gooddaymsc@naver.com <gooddaymsc@naver.com>
2021-09-23 15:38:30 +0900

    sc message

--- app/src/main/java/com/eomcs/pms/handler/MessageListHandler.java
+++ app/src/main/java/com/eomcs/pms/handler/MessageListHandler.java
@@ -31,11 +31,6 @@ public class MessageListHandler extends AbstractMessageHandler {
       System.out.println("받은 메세지가 없습니다.");
       return;
     }
-    //    Message checkedIdmessage = findMessageById(App.getLoginUser().getId());
-    //    if (checkedIdmessage == null) {
-    //      System.out.println("받은 메세지가 없습니다.");
-    //      return;
-    //    }
 
     for (Message message : messageList.getMessage()) {
       System.out.printf("1. 보낸사람 :%s\n", message.getWriter());
@@ -53,33 +48,9 @@ public class MessageListHandler extends AbstractMessageHandler {
       switch (choose) {
         case "0" : return;
         case "1" : request.getRequestDispatcher("/message/add").forward(request); return;
-        //        case "2" : request.getRequestDispatcher("/message/delete").forward(request); return;
+        case "2" : request.getRequestDispatcher("/message/delete").forward(request); return;
         default : System.out.println("잘못입력하셨습니다"); continue;
       }
-
-      //      while(true) {
-      //        for (Message message : messageList) {
-      //
-      //          if (message.equals(findMessageById(App.getLoginUser().getId()))) {
-      //            System.out.printf("1. 보낸사람 :%s\n", message.getWriter());
-      //            System.out.printf("2. 제목 :%s\n", message.getTitle());
-      //            System.out.printf("3. 내용 :%s\n", message.getContent());
-      //            System.out.printf("4. 보낸 날짜 :%s\n", message.getRegistrationDate());
-      //            System.out.println("------------------------------------");
-      //          }
-      //          //        request.setAttribute("writer", message.getWriter());
-      //        }
-      //
-      //        System.out.println("\n< 1.답장 / 2.삭제 / 0.이전  >");
-      //        String choose = Prompt.inputString("선택 > ");
-      //
-      //        switch (choose) {
-      //          case "0" : return;
-      //          case "1" : request.getRequestDispatcher("/message/add").forward(request); return;
-      //          //        case "2" : request.getRequestDispatcher("/message/delete").forward(request); return;
-      //          default : System.out.println("잘못입력하셨습니다"); continue;
-      //        }
-      //      }
     }
   }
 }
