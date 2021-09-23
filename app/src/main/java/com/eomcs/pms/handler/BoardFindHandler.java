c8e7b8c08fbb3b2b49ad733095f4e88b80b5019c
unknown <evahong94@gmail.com>
2021-09-23 20:02:33 +0900

    'My_Board_Comment'

--- app/src/main/java/com/eomcs/pms/handler/BoardFindHandler.java
+++ app/src/main/java/com/eomcs/pms/handler/BoardFindHandler.java
@@ -3,6 +3,7 @@ package com.eomcs.pms.handler;
 import java.util.List;
 import com.eomcs.pms.App;
 import com.eomcs.pms.domain.Board;
+import com.eomcs.util.Prompt;
 
 public class BoardFindHandler implements Command {
   List<Board> boardList;
@@ -15,19 +16,33 @@ public class BoardFindHandler implements Command {
   }
 
   @Override
-  public void execute(CommandRequest request) {
-    System.out.println("[내가 남긴 게시글 목록]");
-    System.out.printf("%-3s\t%-10s\t%-3s\t%-6s\n",
-        "번호","제목","댓글수","등록일");
-    System.out.println("--------------------------------------------------------------------------");
+  public void execute(CommandRequest request) throws Exception {
+    Loop : while(true) {
+      System.out.println("[내가 남긴 게시글 목록]");
+      System.out.printf("%-3s\t%-10s\t%-3s\t%-6s\n",
+          "번호","제목","댓글수","등록일");
+      System.out.println("--------------------------------------------------------------------------");
 
-    for (Board board : boardList) {
-      if (board.getWriter().equals(App.getLoginUser().getId())) {
-        System.out.printf("%-3d\t%-10s\t%-3d\t%-6s\n", 
-            board.getBoardNumber(),
-            board.getTitle(),
-            board.getCommentList().size(),
-            board.getRegistrationDate());
+      for (Board board : boardList) {
+        if (board.getWriter().equals(App.getLoginUser().getId())) {
+          System.out.printf("%-3d\t%-10s\t%-3d\t%-6s\n", 
+              board.getBoardNumber(),
+              board.getTitle(),
+              board.getCommentList().size(),
+              board.getRegistrationDate());
+        }
+      }
+
+      System.out.println("\n1. 상세보기 / 2. 검색 / 이전(0)");
+      while (true) {
+        String choose = Prompt.inputString("선택 > ");
+        System.out.println();
+        switch (choose) {
+          case "0" : return;
+          case "1" : request.getRequestDispatcher("/board/detail").forward(request); continue Loop;
+          case "2" : request.getRequestDispatcher("/board/search").forward(request); continue Loop;
+          default : System.out.println("잘못입력하셨습니다."); continue;
+        }
       }
     }
   }
