9d75f2346110821a13c3c036eadc1a55953860c5
Sumin-yun <sumin.yun73@gmail.com>
2021-10-14 19:13:48 +0900

    erd diagram

--- app-server/src/main/java/com/eomcs/pms/domain/Seller.java
+++ app-server/src/main/java/com/eomcs/pms/domain/Seller.java
@@ -6,38 +6,38 @@ public class Seller extends Member {
   private String businessNumber;
   private String businessAddress;
   private String businessPlaceNumber;
-  private int businessOpeningHours;
-  private int businessOpeningMinutes;
-  private int businessClosingHours;
-  private int businessClosingMinutes;
+  //  private int businessOpeningHours;
+  //  private int businessOpeningMinutes;
+  //  private int businessClosingHours;
+  //  private int businessClosingMinutes;
   private String businessOpeningTime;
   private String businessClosingTime;
 
 
-  public int getBusinessOpeningHours() {
-    return businessOpeningHours;
-  }
-  public void setBusinessOpeningHours(int businessOpeningHours) {
-    this.businessOpeningHours = businessOpeningHours;
-  }
-  public int getBusinessOpeningMinutes() {
-    return businessOpeningMinutes;
-  }
-  public void setBusinessOpeningMinutes(int businessOpeningMinutes) {
-    this.businessOpeningMinutes = businessOpeningMinutes;
-  }
-  public int getBusinessClosingHours() {
-    return businessClosingHours;
-  }
-  public void setBusinessClosingHours(int businessClosingHours) {
-    this.businessClosingHours = businessClosingHours;
-  }
-  public int getBusinessClosingMinutes() {
-    return businessClosingMinutes;
-  }
-  public void setBusinessClosingMinutes(int businessClosingMinutes) {
-    this.businessClosingMinutes = businessClosingMinutes;
-  }
+  //  public int getBusinessOpeningHours() {
+  //    return businessOpeningHours;
+  //  }
+  //  public void setBusinessOpeningHours(int businessOpeningHours) {
+  //    this.businessOpeningHours = businessOpeningHours;
+  //  }
+  //  public int getBusinessOpeningMinutes() {
+  //    return businessOpeningMinutes;
+  //  }
+  //  public void setBusinessOpeningMinutes(int businessOpeningMinutes) {
+  //    this.businessOpeningMinutes = businessOpeningMinutes;
+  //  }
+  //  public int getBusinessClosingHours() {
+  //    return businessClosingHours;
+  //  }
+  //  public void setBusinessClosingHours(int businessClosingHours) {
+  //    this.businessClosingHours = businessClosingHours;
+  //  }
+  //  public int getBusinessClosingMinutes() {
+  //    return businessClosingMinutes;
+  //  }
+  //  public void setBusinessClosingMinutes(int businessClosingMinutes) {
+  //    this.businessClosingMinutes = businessClosingMinutes;
+  //  }
   public String getBusinessName() {
     return businessName;
   }
