59f72ec0456b8f8422caab2d38e0cec5a659f76c
unknown <evahong94@gmail.com>
2021-10-18 18:48:28 +0900

    'point'

--- app-client/src/main/java/com/eomcs/pms/domain/Buyer.java
+++ app-client/src/main/java/com/eomcs/pms/domain/Buyer.java
@@ -1,8 +1,13 @@
 package com.eomcs.pms.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SuppressWarnings("serial")
 public class Buyer extends Member {
   private String address;
+  private int point;
+  private List<Coupon> coupon = new ArrayList<>();
 
   public String getAddress() {
     return address;
@@ -10,4 +15,17 @@ public class Buyer extends Member {
   public void setAddress(String address) {
     this.address = address;
   }
+  public int getPoint() {
+    return point;
+  }
+  public void setPoint(int point) {
+    this.point = point;
+  }
+  public List<Coupon> getCoupon() {
+    return coupon;
+  }
+  public void setCoupon(List<Coupon> coupon) {
+    this.coupon = coupon;
+  }
+
 }
