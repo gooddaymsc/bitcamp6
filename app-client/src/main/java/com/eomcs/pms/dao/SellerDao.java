36ca8c0522c55ee091cf4301f10b1277345cef00
unknown <evahong94@gmail.com>
2021-10-22 18:13:55 +0900

    'Seller_DAO'

--- app-client/src/main/java/com/eomcs/pms/dao/SellerDao.java
+++ app-client/src/main/java/com/eomcs/pms/dao/SellerDao.java
@@ -1,7 +1,6 @@
 package com.eomcs.pms.dao;
 
 import java.util.List;
-import org.apache.ibatis.annotations.Param;
 import com.eomcs.pms.domain.Member;
 import com.eomcs.pms.domain.Seller;
 
@@ -11,9 +10,5 @@ public interface SellerDao {
   Seller findById(String id) throws Exception;
   void update(Seller seller) throws Exception;
   void delete(Seller seller) throws Exception;
-  void insertSeller(@Param("memberNo") int memberNo, @Param("businessName") String businessName, 
-      @Param("businessNumber") String businessNumber, @Param("businessClosingTime") String businessAddress, 
-      @Param("businessPlaceNumber") String businessPlaceNumber, 
-      @Param("businessOpeningTime") String businessOpeningTime, 
-      @Param("businessClosingTime") String businessClosingTime) throws Exception;
+  void insertSeller(Seller seller) throws Exception;
 }
