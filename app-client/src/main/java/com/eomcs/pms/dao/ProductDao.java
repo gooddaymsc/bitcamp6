46260486830c200e611e76ec417a3fd9390081c1
Sumin-yun <sumin.yun73@gmail.com>
2021-10-26 03:00:42 +0900

    Dao-cart update,delete

--- app-client/src/main/java/com/eomcs/pms/dao/ProductDao.java
+++ app-client/src/main/java/com/eomcs/pms/dao/ProductDao.java
@@ -8,17 +8,8 @@ public interface ProductDao {
   void insert(Product product) throws Exception;
   List<Product> findAll() throws Exception;
   Product findByNo(int no) throws Exception;
-  //  // Product findByNo2(int reviewNo) throws Exception;
   Product findByProduct(String name) throws Exception;
   void update(Product product) throws Exception;
   void delete(Product product) throws Exception;
-  //  void insertReview(Review review) throws Exception;
-  //  List<Review> findAll(int productNumber) throws Exception;
-  //  void updateReview(Review review) throws Exception;    
-  //  void deleteReview(Review review) throws Exception;
-  //  HashMap<String,Seller> findByAdress(String address) throws Exception;
-  //  Stock findStockById(String id, int productNumber) throws Exception;
-  //  boolean reviewIs(int productNumber, String id) throws Exception;
-  //  boolean findPurchased(String productName) throws Exception;
   List<ProductType> findAllProductType() throws Exception;
 }
