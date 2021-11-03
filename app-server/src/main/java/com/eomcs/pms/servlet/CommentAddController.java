8f1d944ae13ca0fdb7f6c52616146074bc1d9d2c
unknown <evahong94@gmail.com>
2021-11-03 21:04:35 +0900

    'comment'

--- app-server/src/main/java/com/eomcs/pms/servlet/CommentAddController.java
+++ app-server/src/main/java/com/eomcs/pms/servlet/CommentAddController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.ibatis.session.SqlSession;
 import com.eomcs.pms.dao.CommentDao;
-import com.eomcs.pms.domain.Board;
+import com.eomcs.pms.dao.MemberDao;
 import com.eomcs.pms.domain.Comment;
 import com.eomcs.pms.domain.Member;
 
@@ -20,6 +20,7 @@ public class CommentAddController extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   CommentDao commentDao;
+  MemberDao memberDao;
   SqlSession sqlSession;
 
   @Override
@@ -27,28 +28,25 @@ public class CommentAddController extends HttpServlet {
     ServletContext 웹애플리케이션공용저장소 = config.getServletContext();
     sqlSession = (SqlSession) 웹애플리케이션공용저장소.getAttribute("sqlSession");
     commentDao = (CommentDao) 웹애플리케이션공용저장소.getAttribute("commentDao");
+    memberDao = (MemberDao) 웹애플리케이션공용저장소.getAttribute("memberDao");
+
   }
 
   @Override
   protected void service(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
-    Comment comment = new Comment();
-    comment.setContent(request.getParameter("content"));
-
-    // 로그인 구현 전엔 자신의 가상 데이터에 맞게 setNumber넣으세요..
-    Member member = new Member();
-    member.setNumber(10);
-    comment.setWriter(member);
-
-    Board board = new Board();
-    board.setBoardNumber(1);
-    comment.setBoardNumber(board.getBoardNumber());
-
     try {
+      Comment comment = new Comment();
+      int boardNo = Integer.parseInt(request.getParameter("boardNumber"));
+      comment.setBoardNumber(boardNo);
+      comment.setContent(request.getParameter("content"));
+      Member member = memberDao.findById(request.getParameter("writer"));
+      comment.setWriter(member);
       commentDao.insert(comment);
       sqlSession.commit();
-      response.setHeader("Refresh", "1;url=list");
+
+      response.setHeader("Refresh", "1;url=../board/detail?no="+boardNo);
       request.getRequestDispatcher("comment/CommentAdd.jsp").forward(request, response);
 
     } catch(Exception e){
