d7760dea3c7c731c71c409539cab3663b8898ccc
unknown <evahong94@gmail.com>
2021-11-04 01:02:49 +0900

    'Product'

--- app-server/src/main/java/com/eomcs/pms/servlet/ProductAddController.java
+++ app-server/src/main/java/com/eomcs/pms/servlet/ProductAddController.java
@@ -31,7 +31,6 @@ public class ProductAddController extends HttpServlet {
   protected void service(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
-    Product product = new Product();
     //상품명
     //주종-상세주종
     //원산지
@@ -42,40 +41,40 @@ public class ProductAddController extends HttpServlet {
     //산도(1-5)
     //바디감    
 
-    product.setProductName(request.getParameter("productName"));
-    ProductType productType = new ProductType();
-    productType.setType(request.getParameter("type"));
-    productType.setSubType(request.getParameter("subType"));
-    String type = request.getParameter("type");
-    String subType = request.getParameter("subType");
-
     try {
+      Product product = new Product();
+      product.setProductName(request.getParameter("productName"));
+      String subType = request.getParameter("subType");
+      System.out.println(subType);
+      String type = request.getParameter("type");
+      System.out.println(type);
+
+      ProductType productType = new ProductType();
+      productType.setType(type);
+      productType.setSubType(subType);
+
       product.setProductType(new ProductHandlerHelper(productDao).promptType(type, subType));
-    } catch (Exception e1) {
-      e1.printStackTrace();
-    }
 
-    product.setCountryOrigin(request.getParameter("countryOrigin"));
+      product.setCountryOrigin(request.getParameter("countryOrigin"));
 
-    if(product.getProductType().getType().equals("와인")) {
-      product.setVariety(request.getParameter("variety"));
-    }
+      if(product.getProductType().getType().equals("와인")) {
+        product.setVariety(request.getParameter("variety"));
+      }
 
-    product.setVolume(Integer.parseInt(request.getParameter("volume")));
-    product.setAlcoholLevel(Float.parseFloat(request.getParameter("alcoholLevel")));
-    product.setSugarLevel(Integer.parseInt(request.getParameter("sugarLevel")));
-    product.setAcidity(Integer.parseInt(request.getParameter("acidity")));
-    product.setWeight(Integer.parseInt(request.getParameter("weight")));
+      product.setVolume(Integer.parseInt(request.getParameter("volume")));
+      product.setAlcoholLevel(Float.parseFloat(request.getParameter("alcoholLevel")));
+      product.setSugarLevel(Integer.parseInt(request.getParameter("sugarLevel")));
+      product.setAcidity(Integer.parseInt(request.getParameter("acidity")));
+      product.setWeight(Integer.parseInt(request.getParameter("weight")));
 
-    //    String type = ProductValidation.checkType(request.getParameter("type"));
-    //    String subType = ProductValidation.checkSubType(request.getParameter("subType"));
-    //    product.setProductType(new ProductHandlerHelper(productDao).promptType(type, subType));
+      //    String type = ProductValidation.checkType(request.getParameter("type"));
+      //    String subType = ProductValidation.checkSubType(request.getParameter("subType"));
+      //    product.setProductType(new ProductHandlerHelper(productDao).promptType(type, subType));
 
-    try {
       productDao.insert(product);
       sqlSession.commit();
       response.setHeader("Refresh", "1;url=list");
-      request.getRequestDispatcher("product/ProductAdd.jsp").forward(request, response);
+      request.getRequestDispatcher("ProductAdd.jsp").forward(request, response);
     } catch(Exception e){
       request.setAttribute("error", e);
       request.getRequestDispatcher("/Error.jsp").forward(request, response);   
