535b642ed2f88ffc8aa3b559a11ccde24e496db5
unknown <evahong94@gmail.com>
2021-11-07 22:16:10 +0900

    'member'

--- app-server/src/main/java/com/eomcs/pms/servlet/ProductPlaceController.java
+++ app-server/src/main/java/com/eomcs/pms/servlet/ProductPlaceController.java
@@ -1,7 +1,6 @@
 package com.eomcs.pms.servlet;
 
 import java.io.IOException;
-import java.util.Collection;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -10,7 +9,6 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import com.eomcs.pms.dao.StockDao;
-import com.eomcs.pms.domain.Stock;
 
 @WebServlet("/product/place")
 public class ProductPlaceController extends HttpServlet {
@@ -32,9 +30,9 @@ public class ProductPlaceController extends HttpServlet {
     try {
       String input = request.getParameter("place");
       System.out.println(input);
-
-      Collection<Stock> stockSellerList = (Collection<Stock>)request.getParameter("stockSellerList");
-      request.setAttribute("productList", productList);
+      //
+      //      Collection<Stock> stockSellerList = (Collection<Stock>)request.getParameter("stockSellerList");
+      //      request.setAttribute("productList", productList);
       request.getRequestDispatcher("ProductList.jsp").forward(request, response);
     } catch (Exception e) {
       request.setAttribute("error", e);
