7988ef938d871aa610e09b21bc5b7c2a99dbfc9c
MM&KK <gooddaymsc@naver.com>
2021-11-10 23:52:46 +0900

    ..

--- app-server/src/main/java/com/eomcs/pms/servlet/BuyerAddController.java
+++ app-server/src/main/java/com/eomcs/pms/servlet/BuyerAddController.java
@@ -2,9 +2,9 @@ package com.eomcs.pms.servlet;
 
 import java.io.IOException;
 import java.sql.Date;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +14,7 @@ import com.eomcs.pms.dao.BuyerDao;
 import com.eomcs.pms.domain.Buyer;
 import com.eomcs.pms.domain.Member;
 
+@MultipartConfig(maxFileSize = 1024 * 1024 * 10)
 @WebServlet("/buyer/add")
 public class BuyerAddController extends HttpServlet {
   private static final long serialVersionUID = 1L;
@@ -22,8 +23,8 @@ public class BuyerAddController extends HttpServlet {
   BuyerDao buyerDao;
 
   @Override
-  public void init(ServletConfig config) throws ServletException {
-    ServletContext 웹애플리케이션공용저장소 = config.getServletContext();
+  public void init() {
+    ServletContext 웹애플리케이션공용저장소 = getServletContext();
     sqlSession = (SqlSession) 웹애플리케이션공용저장소.getAttribute("sqlSession");
     buyerDao = (BuyerDao) 웹애플리케이션공용저장소.getAttribute("buyerDao");
   }
@@ -32,48 +33,66 @@ public class BuyerAddController extends HttpServlet {
   public void service(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
-    Buyer buyer = new Buyer();
-    Member member = new Member();
-
-    member.setAuthority(2);
-    member.setId(request.getParameter("id"));
-    member.setName(request.getParameter("name"));
-    member.setNickname(request.getParameter("nickname"));
-    member.setEmail(request.getParameter("email"));
-    member.setBirthday(Date.valueOf(request.getParameter("birthday")));
-    member.setPassword(request.getParameter("password"));
-    member.setPhoto(request.getParameter("photo"));
-    member.setPhoneNumber(request.getParameter("phoneNumber"));
-
-    //    if (findDeletedByName(buyer.getName()) != -1) {
-    //      if (deletedMemberList.get(memberPrompt.findDeletedByName(buyer.getName())).getPhoneNumber().equals(buyer.getPhoneNumber()) && 
-    //          deletedMemberList.get(memberPrompt.findDeletedByName(buyer.getName())).getName().equals(buyer.getName())) {
-    //        System.out.println("탈퇴한 회원입니다. 7일후 재가입해주세요.");
-    //        return;
-    //      }
-    //    }
-
-    // 회원가입할때 쿠폰지급하기 (2개)
-    //      Coupon coupon = new Coupon();
-    //      coupon.setMinimum(10000);
-    //      coupon.setPercent(10);
-    //      buyer.getCoupon().add(coupon);
-    //      coupon = new Coupon();
-    //      coupon.setMinimum(15000);
-    //      coupon.setPrice(2000);
-    //      buyer.getCoupon().add(coupon);
-
-    buyer.setZipcode(request.getParameter("zipcode"));
-    buyer.setAddress(request.getParameter("address"));
-    buyer.setDetailAddress(request.getParameter("detailAddress"));
-    //    System.out.printf("이름 : %s\n", memberPrompt.findById(id).getName());
-    //    member.setRegisteredDate(new Date(System.currentTimeMillis()));
-    //    buyer.setNumber(totalNumberList.get(App.MEMBER_NUMBER_INDEX));
-    //    totalNumberList.set(App.MEMBER_NUMBER_INDEX, buyer.getNumber()+1);
-    //    memberList.add(buyer);
-    buyer.setMember(member);
-
     try {
+      Buyer buyer = new Buyer();
+      Member member = new Member();
+
+      member.setAuthority(2);
+      member.setId(request.getParameter("id"));
+      member.setName(request.getParameter("name"));
+      member.setNickname(request.getParameter("nickname"));
+      member.setEmail(request.getParameter("email"));
+      member.setBirthday(Date.valueOf(request.getParameter("birthday")));
+      member.setPassword(request.getParameter("password"));
+      member.setPhoneNumber(request.getParameter("phoneNumber"));
+      member.setPhoto(request.getParameter("photo"));
+
+      //      Part photoPart = request.getPart("photo");
+      //      if (photoPart.getSize() > 0) {
+      //        String filename = UUID.randomUUID().toString();
+      //        photoPart.write(getServletContext().getRealPath("/upload/buyer") + "/" + filename);
+      //        member.setPhoto(filename);
+      //
+      //        Thumbnails.of(getServletContext().getRealPath("/upload/buyer") + "/" + filename)
+      //        .size(100, 100)
+      //        .outputFormat("jpg")
+      //        .crop(Positions.CENTER)
+      //        .toFiles(new Rename() {
+      //          @Override
+      //          public String apply(String name, ThumbnailParameter param) {
+      //            return name + "_100x100";
+      //          }
+      //        });
+      //    }
+
+      //    if (findDeletedByName(buyer.getName()) != -1) {
+      //      if (deletedMemberList.get(memberPrompt.findDeletedByName(buyer.getName())).getPhoneNumber().equals(buyer.getPhoneNumber()) && 
+      //          deletedMemberList.get(memberPrompt.findDeletedByName(buyer.getName())).getName().equals(buyer.getName())) {
+      //        System.out.println("탈퇴한 회원입니다. 7일후 재가입해주세요.");
+      //        return;
+      //      }
+      //    }
+
+      // 회원가입할때 쿠폰지급하기 (2개)
+      //      Coupon coupon = new Coupon();
+      //      coupon.setMinimum(10000);
+      //      coupon.setPercent(10);
+      //      buyer.getCoupon().add(coupon);
+      //      coupon = new Coupon();
+      //      coupon.setMinimum(15000);
+      //      coupon.setPrice(2000);
+      //      buyer.getCoupon().add(coupon);
+
+      buyer.setZipcode(request.getParameter("zipcode"));
+      buyer.setAddress(request.getParameter("address"));
+      buyer.setDetailAddress(request.getParameter("detailAddress"));
+      //    System.out.printf("이름 : %s\n", memberPrompt.findById(id).getName());
+      //    member.setRegisteredDate(new Date(System.currentTimeMillis()));
+      //    buyer.setNumber(totalNumberList.get(App.MEMBER_NUMBER_INDEX));
+      //    totalNumberList.set(App.MEMBER_NUMBER_INDEX, buyer.getNumber()+1);
+      //    memberList.add(buyer);
+      buyer.setMember(member);
+
       buyerDao.insert(buyer);
       sqlSession.commit();
       request.setAttribute("pageTitle", "회원가입(구매자)");
