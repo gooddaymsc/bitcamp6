4cc303292e2a749a43d2027d479f2efeaf1424e1
jylion23 <jylion23@github.com>
2021-11-17 16:48:48 +0900

    navbar

--- app-client/docs/dbmodel/data.sql
+++ app-client/docs/dbmodel/data.sql
@@ -3,7 +3,7 @@ set foreign_key_checks =0;
 
 -- 관리자 등록
 insert into member(id, password,authority,name,nickname,email,phoneNumber,level) 
-values("admin",password("0000"),8,"admin","admin","admin@test.com","1111",5);
+values("admin",password("1111"),8,"admin","admin","admin@test.com","1111",5);
 
 -- 회원 등록
 insert into member(authority,id,name,nickname,email,birthday,password,photo,phoneNumber,zipcode,address,detail_address) 
@@ -46,7 +46,7 @@ insert into board_tag(board_no, tag_no) values(31, 1);
 insert into tag(tag) values ('');
 
 --auto_increment
-ALTER TABEL tag MODIFY tag_no INT NOT NULL AUTO_INCREMENT;
+ALTER TABLE tag MODIFY tag_no INT NOT NULL AUTO_INCREMENT;
 
 --
 alter table product add photo varchar(255) not null;
